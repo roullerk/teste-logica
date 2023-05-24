b98434b33995aa17612cf770afe9b41fa6bdeab6
VictorFRS <victorfrds@gmail.com>
2023-05-24 13:52:33 -0300

    Teste realizado

--- productStructure.js
+++ productStructure.js
@@ -14,5 +14,23 @@ const products = [
 ]
 
 module.exports = () => {
-	// Seu código vai aqui!
+  const productsObj = {}
+
+  for (let i = 0; i < products.length; i++) {
+    const product = products[i];
+    const [color, size] = product.split("-")
+    if (productsObj.hasOwnProperty(color)) {
+      if (productsObj[color].hasOwnProperty(size)) {
+        productsObj[color][size] += 1;
+      } else {
+        productsObj[color][size] = 1;
+      }
+    }
+    else {
+      productsObj[color] = {
+        [size]: 1
+      }
+    }
+  }
+  return productsObj;
 }
